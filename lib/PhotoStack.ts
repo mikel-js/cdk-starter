01a1d7d4201a373fdd92ddae21e6949dc06bd296
mikel-js <michaelcastro.js@gmail.com>
2023-10-11 23:22:57 +0300

    add default child

--- lib/PhotoStack.ts
+++ lib/PhotoStack.ts
@@ -1,13 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class PhotoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new Bucket(this, 'PhotoBucket', {
+    const myBucket = new Bucket(this, 'PhotoBucket2', {
       bucketName: 'photosbucket-17',
     });
+
+    (myBucket.node.defaultChild as CfnBucket).overrideLogicalId(
+      'PhotoBucket217'
+    );
   }
 }
