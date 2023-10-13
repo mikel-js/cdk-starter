84320b159c0477a0839966dc4a0efb0b245f6681
mikel-js <michaelcastro.js@gmail.com>
2023-10-13 22:37:14 +0300

    add lambda function

--- lib/PhotosHandlerStack.ts
+++ lib/PhotosHandlerStack.ts
@@ -1,13 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
-import { Function as LambdaFunction } from 'aws-cdk-lib/aws-lambda';
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from 'aws-cdk-lib/aws-lambda';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class PhotosHandlerStack extends cdk.Stack {
-  private targetBucket = cdk.Fn.importValue('photos-bucket');
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    new LambdaFunction();
+    const targetBucket = cdk.Fn.importValue('photos-bucket');
+    new LambdaFunction(this, 'PhotosHandler', {
+      runtime: Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: Code.fromInline(`
+      exports.handler = async (event) => {
+        console.log("hello!zxzxc: " + process.env.TARGET_BUCKET)
+      };
+    `),
+      environment: {
+        TARGET_BUCKET: targetBucket,
+      },
+    });
   }
 }
