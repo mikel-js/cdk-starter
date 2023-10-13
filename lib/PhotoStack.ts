d60ecddceb212949ead96b66750dd208558c2f96
mikel-js <michaelcastro.js@gmail.com>
2023-10-13 22:34:48 +0300

    Add bucket links

--- lib/PhotoStack.ts
+++ lib/PhotoStack.ts
@@ -1,17 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
+import { Fn, CfnOutput } from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class PhotoStack extends cdk.Stack {
+  private stackSuffix: string;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myBucket = new Bucket(this, 'PhotoBucket2', {
-      bucketName: 'photosbucket-1722',
+    this.initializeSiffux();
+
+    const photosBucket = new Bucket(this, 'PhotoBucket2', {
+      bucketName: `photos-bucket-${this.stackSuffix}`,
     });
 
-    (myBucket.node.defaultChild as CfnBucket).overrideLogicalId(
-      'PhotoBucket217'
-    );
+    new CfnOutput(this, 'photos-bucket', {
+      value: photosBucket.bucketArn,
+      exportName: 'photos-bucket',
+    });
+  }
+  private initializeSiffux() {
+    const shortStackId = Fn.select(2, Fn.split('/', this.stackId));
+    this.stackSuffix = Fn.select(4, Fn.split('-', shortStackId));
   }
 }
--- lib/PhotosHandlerStack.ts
+++ lib/PhotosHandlerStack.ts
@@ -1,17 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
+import { Function as LambdaFunction } from 'aws-cdk-lib/aws-lambda';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class PhotosHandlerStack extends cdk.Stack {
+  private targetBucket = cdk.Fn.importValue('photos-bucket');
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myBucket = new Bucket(this, 'PhotoBucket2', {
-      bucketName: 'photosbucket-1722',
-    });
-
-    (myBucket.node.defaultChild as CfnBucket).overrideLogicalId(
-      'PhotoBucket217'
-    );
+    new LambdaFunction();
   }
 }
