b3a24dc2c300c81fc721bcabe6cd82eaa3cec3db
mikel-js <michaelcastro.js@gmail.com>
2023-10-13 22:48:02 +0300

    add new stak to cdk starter

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PhotoStack } from '../lib/PhotoStack';
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
 
 const app = new cdk.App();
-new PhotoStack(app, 'PhotoStack', {});
+new PhotoStack(app, 'PhotoStack');
+new PhotosHandlerStack(app, 'PhotosHandlerStack');
