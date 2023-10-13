36856638e08418b4038748a75f3be10f5a5f2229
mikel-js <michaelcastro.js@gmail.com>
2023-10-13 22:18:27 +0300

    Add phot handler stack

--- lib/PhotosHandlerStack.ts
+++ lib/PhotosHandlerStack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
-export class PhotoStack extends cdk.Stack {
+export class PhotosHandlerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
