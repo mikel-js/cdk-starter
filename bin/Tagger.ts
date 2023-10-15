3604f12e212a6289ac16359b4e514a529addb3ee
mikel-js <michaelcastro.js@gmail.com>
2023-10-15 21:54:21 +0300

    import tagger in cdk starter

--- bin/Tagger.ts
+++ bin/Tagger.ts
@@ -1,10 +1,14 @@
 import { IAspect } from 'aws-cdk-lib';
+import { CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { IConstruct } from 'constructs';
 
 export class BucketTagger implements IAspect {
   constructor(private key: string, private value: string) {}
 
   visit(node: IConstruct): void {
-    throw new Error('Method not implemented');
+    console.log(`visiting: ${node.node.id}`);
+    if (node instanceof CfnBucket) {
+      node.tags.setTag(this.key, this.value);
+    }
   }
 }
--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -3,9 +3,13 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PhotoStack } from '../lib/PhotoStack';
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTagger } from './Tagger';
 
 const app = new cdk.App();
 const photoStack = new PhotoStack(app, 'PhotoStack');
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
   targetBucketArn: photoStack.photoBucketArn,
 });
+
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
